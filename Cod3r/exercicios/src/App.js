f3545bc34d3c51b352e8ae8112f34bacd8d7996b
appwork-br <app_work@hotmail.com>
2021-12-05 15:29:17 -0300

    React Fragment

--- Cod3r/exercicios/src/App.js
+++ Cod3r/exercicios/src/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Primeiro from './components/Primeito'
 import CompPadrao, { Comp1, Comp2 } from './components/Multi'
+import MinMax from './components/MinMax'
+import Aleatorio from './components/Aleatorio';
+import Titulo from './components/Titulo';
+import Botao from './components/Botao'
 
 export default () => (
     <View style={style.App}>
+        <Botao />
+        {/* <Titulo principal="Cadastro de Produto"
+            secundario="Tela de Cadastro do Produto"/>
+        <Aleatorio min={1} max={60}/>
+        <Aleatorio min={1} max={60}/>
+        <Aleatorio min={1} max={60}/>
+        <Aleatorio min={1} max={60}/>
+        <Aleatorio min={1} max={60}/>
+        <Aleatorio min={1} max={60}/>
+        <MinMax min={3} max={20}/>
+        <MinMax min={1} max={94}/>
         <CompPadrao />
         <Comp1 />
         <Comp2 />
-        <Primeiro />
+        <Primeiro /> */}
     </View>
 )
 
@@ -17,5 +32,6 @@ const style = StyleSheet.create({
         flexGrow: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 20,
     }
 })
