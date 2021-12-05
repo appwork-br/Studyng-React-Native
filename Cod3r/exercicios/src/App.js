f34f0d316b4f4a498765f4ce4714043832377b56
appwork-br <app_work@hotmail.com>
2021-12-05 11:15:36 -0300

    Iniciando novamente

---
+++ Cod3r/exercicios/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Primeiro from './components/Primeito'
+import CompPadrao, { Comp1, Comp2 } from './components/Multi'
+
+export default () => (
+    <View style={style.App}>
+        <CompPadrao />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro />
+    </View>
+)
+
+const style = StyleSheet.create({
+    App: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
